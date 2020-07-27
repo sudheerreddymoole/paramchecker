d4e9ef6d612a0d7eb9198d407216aa063d2f3a66
sudheerreddymoole <68851023+sudheerreddymoole@users.noreply.github.com>
2020-07-27 17:13:28 +0530

    Update paramchecker.cpp
    updating with latest logic

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,22 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  //int i;
-  if(bpm < 70 || bpm > 150) {
+  int i,j;
+  if(bpm < 70)
+    i++;
+  if(bpm > 150) 
+    i++;
+  if(i==2)
     return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
+  else if(spo2 < 80) {
     return false;
   }
+  
+  if(respRate < 30)
+    j++;
+  if(respRate > 60)
+    j++;
+  if(j == 2)
+  return false;
+  
   return true;
 }
